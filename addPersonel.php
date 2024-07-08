84bf720a550e6646310f182f4b0d74f1c842093a
Efedrkoglu <efedurukoglu@gmail.com>
2024-07-08 13:57:34 +0300

    commit

--- addPersonel.php
+++ addPersonel.php
@@ -2,7 +2,60 @@
 <?php include 'header.php'?>
 
 <div class="container mt-5">
-    personel ekle
+    <form>
+        <div class="row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Ad" aria-label="Ad">
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Soy Ad" aria-label="Soy Ad">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <label for="inputState" class="form-label">Department</label>
+                <select id="inputState" class="form-select">
+                    <option>dep1</option>
+                    <option>dep2</option>
+                    <option>dep3</option>
+                    <option>dep3</option>
+                </select>
+            </div>
+            <div class="col">
+                <label for="inputState" class="form-label">Unvan</label>
+                <select id="inputState" class="form-select">
+                    <option>dep1</option>
+                    <option>dep2</option>
+                    <option>dep3</option>
+                    <option>dep3</option>
+                </select>
+            </div>
+            <div class="col">
+                <label for="inputState" class="form-label">Cinsiyet</label>
+                <select id="inputState" class="form-select">
+                    <option>Erkek</option>
+                    <option>Kadın</option>
+                </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Proje" aria-label="Proje">
+            </div>
+            
+        </div>
+        <div class="row">
+            <div class="col">
+                <label>Ise giris tarihi</label>
+                <input type="date">
+            </div>
+            <div class="col">
+                <label>Dogum tarihi</label>
+                <input type="date">
+            </div>
+        </div>
+        <input type="submit" value="Kaydet">
+    </form>
 </div>
 
 <?php include 'footer.php'?>
--- personelList.php
+++ personelList.php
@@ -3,23 +3,55 @@
 <?php include 'header.php'; ?>
 
 <div class="container mt-5">
-    <table class="table table-striped">
-        <tr>
-            <th>Ad</th>
-            <th>Soyad</th>
-        </tr>
-        <tr>
-            <td>deneme1</td>
-            <td>deneme2</td>
-        </tr>
-        <tr>
-            <td>deneme3</td>
-            <td>deneme4</td>
-        </tr>
-        <tr>
-            <td>deneme5</td>
-            <td>deneme6</td>
-        </tr>
+    <table class="table table-striped table-bordered table-hover">
+        <thead>
+            <tr align="center">
+                <th>Ad</th>
+                <th>Soyad</th>
+                <th>Departman</th>
+                <th>Unvan</th>
+                <th>İşe Giriş Tarihi</th>
+                <th>İzin Tarihi</th>
+                <th>Dogum Tarihi</th>
+                <th>Cinsiyet</th>
+                <th>Projesi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr align="center">
+                <td>deneme1</td>
+                <td>deneme2</td>
+                <td>deneme3</td>
+                <td>deneme4</td>
+                <td>deneme5</td>
+                <td>deneme6</td>
+                <td>deneme7</td>
+                <td>deneme8</td>
+                <td>deneme9</td>
+            </tr>
+            <tr align="center">
+                <td>deneme1</td>
+                <td>deneme2</td>
+                <td>deneme3</td>
+                <td>deneme4</td>
+                <td>deneme5</td>
+                <td>deneme6</td>
+                <td>deneme7</td>
+                <td>deneme8</td>
+                <td>deneme9</td>
+            </tr>
+            <tr align="center">
+                <td>deneme1</td>
+                <td>deneme2</td>
+                <td>deneme3</td>
+                <td>deneme4</td>
+                <td>deneme5</td>
+                <td>deneme6</td>
+                <td>deneme7</td>
+                <td>deneme8</td>
+                <td>deneme9</td>
+            </tr>
+        </tbody>
     </table>
 </div>
 
