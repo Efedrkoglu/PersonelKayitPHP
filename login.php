910080472d1ef75176572feea11a5ac78f13982f
EmreSoftWareing <030121053@std.izu.edu.tr>
2024-07-16 11:41:53 +0300

    Giriş Ekranı Hata Mesajı

--- login.php
+++ login.php
@@ -1,16 +1,20 @@
 <?php
-    session_start();
-    include('code/DbQuerries.php');
+session_start();
+include('code/DbQuerries.php');
 
-    if(isset($_POST['login'])) {
-        $user = new User(0, $_POST['username'], $_POST['password']);
-        if(authorizeUser($user)) {
-            $_SESSION['loggedin'] = true;
-            $_SESSION['username'] = $user->getUsername();
-            header("Location: pages/home.php");
-            exit();
-        }
-    }    
+$error_message = '';
+
+if(isset($_POST['login'])) {
+    $user = new User(0, $_POST['username'], $_POST['password']);
+    if(authorizeUser($user)) {
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $user->getUsername();
+        header("Location: pages/home.php");
+        exit();
+    } else {
+        $error_message = 'Hatalı kullanıcı adı veya parola!';
+    }
+}    
 ?>
 
 <!DOCTYPE html>
@@ -18,12 +22,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PTS Giris</title>
+    <title>PTS Giriş</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <div class="login-container">
         <h2>Personel Takip Sistemine Hoşgeldiniz</h2>
+        <?php if ($error_message): ?>
+            <p class="error-message"><?php echo $error_message; ?></p>
+        <?php endif; ?>
         <form action="" method="POST">
             <div class="form-group">
                 <label for="username">Kullanıcı Adı:</label>
